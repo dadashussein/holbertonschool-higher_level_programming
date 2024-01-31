11555e88bfcad9151cca3024d456b31e7d4371f7
Dadash <huseynzade.dadas@gmail.com>
2024-01-31 13:37:45 +0400

    update

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -6,13 +6,10 @@ def safe_function(fct, *args):
     result = 0
     try:
         if args:
-           result = fct(*args)
+            result = fct(*args)
         else:
             result = fct()
-    except ZeroDivisionError as e:
-        result = None
+    except (ZeroDivisionError, IndexError) as e:
         print("Exception: ", e, file=sys.stderr)
-    except IndexError as e:
         result = None
-        print("Exception: ", e,  file=sys.stderr)
     return result
