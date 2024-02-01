e8cebaa9ef6d046b2f50922154aa1a1b23bd7c9c
Dadash <huseynzade.dadas@gmail.com>
2024-02-01 16:18:37 +0400

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,31 +4,31 @@
 
 class Square:
     """Document for class"""
+
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        """Document for init method"""
+
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Document for size method"""
         return self.__size
-    
+
     @property
     def position(self):
         return self.__position
-    
+
     @size.setter
     def size(self, value):
-        """Document for setter property"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     @position.setter
     def position(self, value):
-        """Document for setter property"""
         if not (
                 isinstance(value, tuple)
                 and len(value) == 2
@@ -38,19 +38,18 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Document for method"""
         return self.__size ** 2
 
     def my_print(self):
-        """ Print the square"""
-        if not self.size:
+        if self.size == 0:
             print()
         else:
-            for row in range(self.__position[1]):
+            for _ in range(self.position[1]):
                 print()
-            for row in range(self.__size):
-                for column in range(self.__position[0]):
+
+            for _ in range(self.size):
+                for _ in range(self.position[0]):
                     print(" ", end="")
-                for column in range(self.__size):
+                for _ in range(self.size):
                     print("#", end="")
                 print()
