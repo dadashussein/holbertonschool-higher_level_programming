a97f4aa4d4cc77ce92af978354a53092850df174
Dadash <huseynzade.dadas@gmail.com>
2024-02-05 11:58:56 +0400

    set/get atribut on class

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,27 +5,32 @@
 class Rectangle:
     """Document for class"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Document for new obj"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """Get width"""
         return self.__width
-
-    @property
-    def height(self):
-        return self.__height
 
     @width.setter
     def width(self, value):
+        """Set width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
     @height.setter
     def height(self, value):
+        """Set height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
