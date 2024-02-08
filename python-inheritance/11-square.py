da226368ab417efa24a112f8d8be36a381aa298b
Dadash <huseynzade.dadas@gmail.com>
2024-02-08 22:58:36 +0400

    finish

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Document for module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Document for class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[{}] {}/{}".format(Square.__name__, self.__size, self.__size)
