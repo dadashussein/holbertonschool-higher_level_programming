6da5d2f89ec05f1636b42da96de8ec77045fd148
Dadash <huseynzade.dadas@gmail.com>
2024-02-08 21:25:43 +0400

    area implement

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Document for module"""
+
+
+class BaseGeometry:
+    """Document for class"""
+    def area(self):
+        """Calculate area"""
+        raise Exception("area() is not implemented")
