4a1d5aea2a36a95722c0e6a702a010edc1c23e5a
Dadash <huseynzade.dadas@gmail.com>
2024-02-08 19:55:11 +0400

    inherit from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Document for module"""
+
+
+class MyList(list):
+    """Document for class"""
+    def print_sorted(self):
+        """Method list"""
+        print(sorted(self))
