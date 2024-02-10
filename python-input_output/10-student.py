9745cd663474997b8ce7dfe452b6ee9f13dfad97
Dadash <huseynzade.dadas@gmail.com>
2024-02-10 12:53:57 +0400

    filter attr

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Document for module"""
+
+
+class Student:
+    """Document for class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Document for method"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
