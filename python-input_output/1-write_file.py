0c87929246e68f7c8e25fa48f5768a6734942c0e
Dadash <huseynzade.dadas@gmail.com>
2024-02-10 10:29:41 +0400

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Document for function"""
-    with open(filename, 'w+') as file:
-        file.write(text)
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
