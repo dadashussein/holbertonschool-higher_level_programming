2d46f4e35c10985a1088bda021b333217ba79fc9
Dadash <huseynzade.dadas@gmail.com>
2024-02-10 11:14:24 +0400

    save json

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Document for module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Document for function"""
+    json_file = json.dumps(my_obj)
+    with open(filename, "w") as file:
+        return file.write(json_file)
