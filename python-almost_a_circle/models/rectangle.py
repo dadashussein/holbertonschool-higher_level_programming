182bf7ab03d834d09887a6883e29246fe9be9d44
Dadash <huseynzade.dadas@gmail.com>
2024-02-12 23:03:47 +0400

    add compare statment

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, input):
         """set value"""
+        if type(input) is not int:
+            raise TypeError("width must be an integer")
+        if input <= 0:
+            raise ValueError("width must be > 0")
         self.__width = input
 
     @property
@@ -30,6 +34,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, input):
         """doc for setter"""
+        if type(input) is not int:
+            raise TypeError("height must be an integer")
+        if input <= 0:
+            raise ValueError("height must be > 0")
         self.__height = input
 
     @property
@@ -40,6 +48,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, input):
         """doc for setter"""
+        if type(input) is not int:
+            raise TypeError("x must be an integer")
+        if input <= 0:
+            raise ValueError("x must be > 0")
         self.__x = input
 
     @property
@@ -50,4 +62,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, input):
         """doc for setter"""
+        if type(input) is not int:
+            raise TypeError("y must be an integer")
+        if input <= 0:
+            raise ValueError("y must be > 0")
         self.__y = input
