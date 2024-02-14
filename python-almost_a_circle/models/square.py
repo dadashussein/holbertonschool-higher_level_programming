08df7993f9a214e86f22ab7f2aefdcd7c1b82f2d
Dadash <huseynzade.dadas@gmail.com>
2024-02-14 15:38:08 +0400

    update method add derived class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,3 +23,24 @@ class Square(Rectangle):
         """doc for setter"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        if args and len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
