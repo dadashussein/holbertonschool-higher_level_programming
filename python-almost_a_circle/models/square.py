5c0b522122af2bc7bde9fcdd15f1841b81af515e
Dadash <huseynzade.dadas@gmail.com>
2024-02-14 15:24:58 +0400

    add setter & getter my derived class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,3 +12,14 @@ class Square(Rectangle):
     def __str__(self):
         return "[{}] ({}) {}/{} - {}".format(
             Square.__name__, self.id, self.x, self.y, self.size)
+
+    @property
+    def size(self):
+        """doc for getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """doc for setter"""
+        self.width = value
+        self.height = value
