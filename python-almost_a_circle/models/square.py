b9fac7a7e739be70a432a01d8d8537864aa9092a
Dadash <huseynzade.dadas@gmail.com>
2024-02-14 15:10:45 +0400

    creat square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Document for module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Document for Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}".format(
+            Square.__name__, self.id, self.x, self.y, self.size)
