9b144f22c9a356168516b7be90ea8fc28277b934
Dadash <huseynzade.dadas@gmail.com>
2024-02-14 10:48:33 +0400

    add str method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,8 @@ class Rectangle(Base):
             for __ in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self) -> str:
+        return "[{}] ({}) {}/{} - {}/{}".format(Rectangle.__name__, self.id,
+                                                self.__x, self.__y,
+                                                self.__width, self.__height)
