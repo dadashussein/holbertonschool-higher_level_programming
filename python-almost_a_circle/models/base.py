9a9a6e339173639579e76f037dde06ae3245d979
Dadash <huseynzade.dadas@gmail.com>
2024-02-15 10:06:39 +0400

    add class method to save file my json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,3 +19,13 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(list_dicts))
