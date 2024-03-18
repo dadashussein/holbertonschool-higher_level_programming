fdd8d210699a2e3c7a515873f4c2128a9adcdf5d
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-18 15:19:31 +0400

    limit or operator

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,15 @@
+-- list top 3 of cities temperature July and August
+SELECT
+    city,
+    AVG(value) as avg_temp
+FROM
+    temperatures
+WHERE
+    month = 7
+    OR month = 8
+GROUP BY
+    city
+ORDER BY
+    avg_temp DESC
+LIMIT
+    3;
