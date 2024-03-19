f5373bd4945757859e06b3be5d67300fcef5977b
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-19 11:47:01 +0400

    auto_increment

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- crteate database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- select database
+USE hbtn_0d_usa;
+
+-- create table
+CREATE TABLE
+    IF NOT EXISTS states (
+        id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL
+    );
