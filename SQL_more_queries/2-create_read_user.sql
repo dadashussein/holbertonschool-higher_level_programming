5b560786397dcb7ba9c2ea882aa12716f28b86a7
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-19 10:52:24 +0400

    grant select privilege

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,3 +6,6 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- GRANT select privilege 
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+
+--  FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
