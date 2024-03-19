06f63a01e08f79e17ceb1ffa700cb44bc958495b
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-19 10:33:57 +0400

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- give all privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
 
 --  FLUSH PRIVILEGES
 FLUSH PRIVILEGES;
