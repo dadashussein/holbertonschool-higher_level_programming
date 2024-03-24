232392e00fff6ae201ca3142b28ddfe1031509b7
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-24 17:25:14 +0400

    relationship features

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 import sys
 
 
@@ -12,7 +12,6 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
 
     new_state = State(name="California")
