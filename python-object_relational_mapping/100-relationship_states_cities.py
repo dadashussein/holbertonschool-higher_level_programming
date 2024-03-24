2263e441cd751626205d20ba60c4336bc0ca8428
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-24 17:26:31 +0400

    relationship features

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session()
+    session = Session(engine)
 
     new_state = State(name="California")
     new_city = City(name="San Fransico")
