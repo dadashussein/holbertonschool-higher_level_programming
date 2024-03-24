129f6a3f7a29fff5398e04318d30ee2a4b062368
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-24 17:41:37 +0400

    fix

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" Base module"""
+"""State Module"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
 
 Base = declarative_base()
 
@@ -12,6 +11,7 @@ class State(Base):
     """State class"""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
